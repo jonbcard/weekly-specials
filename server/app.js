9412b58436503c1b951498541c3b4a6f1b94dd82
Jonathan Card <jon.card@gmail.com>
2013-02-09 19:34:40 -0700

    Adding google auth in express.

--- server/app.js
+++ server/app.js
@@ -1,27 +1,60 @@
 var express = require('express')
-  , http = require('http')
-  , path = require('path')
-  , passport = require('passport')
-  , user = require('./routes/user')
-  , security = require('./routes/security')
+    , http = require('http')
+    , path = require('path')
+    , passport = require('passport')
+    , user = require('./routes/user')
+    , security = require('./routes/security')
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', 3001);
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+    app.set('port', 3001);
+    app.use(express.favicon());
+    app.use('/static', express.static(path.resolve(__dirname, '../app')));
+
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: 'fancy dancy secret' }));
+
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    app.use(app.router);
+
 });
 
-app.get('/users', user.list);
+security.initialize();
+
+app.get('/login', security.login);
 app.get('/logout', security.logout);
+
+app.get('/auth/google',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+        res.redirect('/');
+    });
+
+app.get('/auth/google/return',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+        res.redirect('/');
+    });
+
+
+app.get('/users', ensureAuthenticated, user.list);
+app.get('/account', ensureAuthenticated, security.getAccount);
 
 app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+    console.log("Express server listening on port " + app.get('port'));
 });
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/login')
+}
--- server/routes/security.js
+++ server/routes/security.js
@@ -1,10 +1,40 @@
-var express = require('express');
-var passport = require('passport');
-var app = express();
+var express = require('express')
+    , passport = require('passport')
+    , GoogleStrategy = require('passport-google').Strategy;
 
 var security = {
     initialize: function() {
+        var port = 3001,
+            host = 'localhost'
+            baseUrl = 'http://' + host + ':' + port;
+
+        passport.serializeUser(function(user, done) {
+            done(null, user);
+        });
+
+        passport.deserializeUser(function(obj, done) {
+            done(null, obj);
+        });
+
+        var request = {
+            returnURL: baseUrl + '/auth/google/return',
+                realm: baseUrl + '/'
+        }
+        console.log(request);
+
+        passport.use(new GoogleStrategy(request,
+            function(identifier, profile, done) {
+                process.nextTick(function () {
+                    profile.identifier = identifier;
+                    return done(null, profile);
+                });
+            }
+        ));
         //passport.use(new MongoStrategy(url, apiKey, dbName, authCollection));
+    },
+
+    getAccount: function(req, res){
+        res.send({ user: req.user });
     },
     login: function(req, res, next) {
         res.send(200);
@@ -14,5 +44,7 @@ var security = {
         res.send(204);
     }
 };
+
+
 
 module.exports = security;
